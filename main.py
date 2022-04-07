084b2a13dddd5a86c44920261d4adca0ce8736df
Angelita Santos <angelitasantos@gmail.com>
2022-04-07 10:14:07 -0300

    alter style python flask

---
+++ main.py
@@ -0,0 +1,22 @@
+from flask import Flask, redirect, render_template
+
+app = Flask(__name__)
+
+@app.errorhandler(404)
+def not_found(e):
+    return redirect('/')
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/gridlayout')
+def grid_layout():
+    return render_template('grid_layout.html')
+
+@app.route('/flexbox')
+def flexbox():
+    return render_template('flexbox.html')
+
+if __name__ == '__main__':
+    app.run(debug = True)
